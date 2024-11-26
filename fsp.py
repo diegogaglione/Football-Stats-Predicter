da35b1f4473e3b74069b0d7b20b7edefff603dd0
Diego <72506891+diegogaglione@users.noreply.github.com>
2024-11-26 14:27:30 +0100

    v 1.0.1
    More data extracted

--- fsp.py
+++ fsp.py
@@ -6,23 +6,27 @@ class fsp_main:
         self.squadre = {}
         self.soup = None
         self.table = None
+        self.no_name_dupe = True
 
     def ottieni_pagina(self):
         page = "https://fbref.com/en/comps/11/Serie-A-Stats"
         html_content = requests.get(page).text
         soup = BeautifulSoup(html_content, "html.parser")
-        self.table = soup.find("table", {"id": "stats_squads_shooting_for"})
+        self.table = soup.find_all("table")[2:]
 
     def ottieni_nome_squadra(self):
-        body = self.table.find("tbody").find_all("tr")
-        for row in body:
-            th = row.find("th")
-            nome = th.find("a", href=True)
-            nome = nome.text
-            self.squadre[nome] = {}
-            th = row.findAll("td")
-            for x in th:
-                self.squadre[nome][x.get("data-stat")] = x.text
+        for y in self.table:
+            body = y.find("tbody").find_all("tr")
+            for row in body:
+                if self.no_name_dupe is True:
+                    th = row.find("th")
+                    nome = th.find("a", href=True)
+                    nome = nome.text
+                    self.squadre[nome] = {}
+                th = row.findAll("td")
+                for x in th:
+                    self.squadre[nome][x.get("data-stat")] = x.text
+            self.no_name_dupe = False
 fsp = fsp_main()
 fsp.ottieni_pagina()
 fsp.ottieni_nome_squadra()
