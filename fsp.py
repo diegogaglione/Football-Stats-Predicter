468807ecca910bd2c3e1c8f53f4bb7e90318cb0f
Diego <72506891+diegogaglione@users.noreply.github.com>
2024-11-27 18:34:28 +0100

    1.0.2
    All data obtained. Ready for a big update

--- fsp.py
+++ fsp.py
@@ -1,33 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-
-class fsp_main:
-    def __init__(self):
-        self.squadre = {}
-        self.soup = None
-        self.table = None
-        self.no_name_dupe = True
-
-    def ottieni_pagina(self):
-        page = "https://fbref.com/en/comps/11/Serie-A-Stats"
-        html_content = requests.get(page).text
-        soup = BeautifulSoup(html_content, "html.parser")
-        self.table = soup.find_all("table")[2:]
-
-    def ottieni_nome_squadra(self):
-        for y in self.table:
-            body = y.find("tbody").find_all("tr")
-            for row in body:
-                if self.no_name_dupe is True:
+squadre = {}
+ids = ['stats_squads_standard_for', 'stats_squads_standard_against', 'stats_squads_keeper_for', 'stats_squads_keeper_against', 'stats_squads_keeper_adv_for', 'stats_squads_keeper_adv_against', 'stats_squads_shooting_for', 'stats_squads_shooting_against', 'stats_squads_passing_for', 'stats_squads_passing_against', 'stats_squads_passing_types_for', 'stats_squads_passing_types_against', 'stats_squads_gca_for', 'stats_squads_gca_against', 'stats_squads_defense_for', 'stats_squads_defense_against', 'stats_squads_possession_for', 'stats_squads_possession_against', 'stats_squads_playing_time_for', 'stats_squads_playing_time_against', 'stats_squads_misc_for', 'stats_squads_misc_against']
+def ottieni_pagina():
+            page = "https://fbref.com/en/comps/11/Serie-A-Stats"
+            html_content = requests.get(page).text
+            soup = BeautifulSoup(html_content, "html.parser")
+            for id in ids:
+                table = soup.find("table", {"id": id})
+                body = table.find("tbody").find_all("tr")
+                for row in body:
                     th = row.find("th")
                     nome = th.find("a", href=True)
                     nome = nome.text
-                    self.squadre[nome] = {}
-                th = row.findAll("td")
-                for x in th:
-                    self.squadre[nome][x.get("data-stat")] = x.text
-            self.no_name_dupe = False
-fsp = fsp_main()
-fsp.ottieni_pagina()
-fsp.ottieni_nome_squadra()
+                    if nome not in squadre:
+                        squadre[nome] = {}
+                    th = row.findAll("td")
+                    for x in th:
+                        if x.get("data-stat") not in squadre[nome]:    
+                            squadre[nome][x.get("data-stat")] = x.text
+            print(squadre["Atalanta"])
+
+ottieni_pagina()
 
